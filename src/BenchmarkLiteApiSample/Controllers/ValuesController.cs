82d28253c7370d88367fd4edb78602f2ebf9687a
Milan Stanacev <milan@stanacev.com>
2016-09-08 17:57:18 +0200

    init

---
+++ src/BenchmarkLiteApiSample/Controllers/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿using LiteApi;
+
+namespace BenchmarkLiteApiSample.Controllers
+{
+    public class ValuesController : LiteController
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ src/BenchmarkWebApiSample/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenchmarkWebApiSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
